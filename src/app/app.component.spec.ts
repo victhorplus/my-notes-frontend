3462800891f26c4143ef1896389b27b5ab29bf8f
Victhor Hugo <victhor-hugo-11@hotmail.com>
2024-06-17 22:00:58 -0300

    feat: Config jest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,15 +2,17 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  var component: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
+    var fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,8 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  title: string;
+  constructor() {
+    this.title = '';
+  }
 }
