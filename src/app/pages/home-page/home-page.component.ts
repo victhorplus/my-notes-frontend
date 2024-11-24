ff604f16efd1446638754083954e8853ffd4bc60
Victhor Hugo <victhor-hugo-11@hotmail.com>
2024-11-24 00:32:55 -0300

    feat: Eventos de listagem na home-page

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NoteListContainerComponent } from './containers';
-import { AccesTokenTimerComponent } from '../../components';
+import { NoteContainerComponent, NoteListContainerComponent } from './containers';
 import { NotesService } from '../../providers/services';
 import { INoteParams, Note } from '../../classes';
 import { finalize, Observable } from 'rxjs';
@@ -9,7 +8,7 @@ import { finalize, Observable } from 'rxjs';
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule, NoteListContainerComponent, AccesTokenTimerComponent],
+  imports: [CommonModule, NoteListContainerComponent, NoteContainerComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
@@ -27,5 +26,16 @@ export class HomePageComponent {
       finalize(() => this.isLoading = false)
     );
   }
+
+  onSelectNote(note: Note): void {
+    console.log("Selecionar anotação:", note);
+  }
+
+  onAddNote(): void {
+    console.log("Adicionar anotação");
+  }
   
+  onDeleteNote(note: Note): void {
+    console.log("Deletar anotação:", note);
+  }
 }
