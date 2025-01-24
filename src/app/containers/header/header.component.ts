7b798271c07cb0df7076692e1c7d34a94ca12119
Victhor Hugo <victhor-hugo-11@hotmail.com>
2025-01-24 15:33:10 -0300

    feat: Cria imagem do usuário com menu de navegacao

--- src/app/containers/header/header.component.ts
+++ src/app/containers/header/header.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule, Router } from '@angular/router';
+import { AuthenticateService } from '../../providers/services';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatMenuModule, MatIconModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  imageSource: string = 'assets/images/default-profile-image.png';
+  isLogged: boolean;
 
+  constructor(private router: Router, private authenticateService: AuthenticateService) {
+    this.isLogged = this.authenticateService.isLogged();
+  }
+  
+  logout(): void {
+    localStorage.clear();
+    this.router.navigate(['/login'])
+  }
 }
